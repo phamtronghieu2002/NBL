8728255e2f1a53110d408126f9885a070a78ce6e
NguyenThinh <koulse.kudaza@gmail.com>
2024-09-11 22:09:21 +0700

    format data

--- src/conponents/modals/ModalImportExel.tsx
+++ src/conponents/modals/ModalImportExel.tsx
@@ -3,7 +3,10 @@ import { ModalCView } from "../ModalC/ModalC"
 import { Button } from "antd"
 import UploadExel from "../../pages/manager/Remind/components/Upload/UploadFile"
 import { FileExcelOutlined } from "@ant-design/icons"
-import { ViahicleProviderContextProps, viahiclesContext } from "../../pages/manager/Remind/providers/ViahicleProvider"
+import {
+  ViahicleProviderContextProps,
+  viahiclesContext,
+} from "../../pages/manager/Remind/providers/ViahicleProvider"
 interface ModalImportExelProps {
   button: React.ReactNode
 }
@@ -12,18 +15,119 @@ const ImportExel: FC<{
   action: any
 }> = ({ action }) => {
   const [isUpload, setIsUpload] = useState<Boolean>(false)
+  const [excelData, setExcelData] = useState<any[]>([])
+
+  const handleImport = () => {
+    //check foramt date
+    excelData.forEach((item, index) => {
+      // Kiểm tra thuộc tính remindDate
+      const remindDate = item.remindDate
+
+      if (!Array.isArray(remindDate)) {
+        console.log(
+          `Item at index ${index} has invalid remindDate format: Not an array`,
+        )
+        return
+      }
+
+      // Kiểm tra từng phần tử trong mảng remindDate
+      remindDate.forEach((date, i) => {
+        if (typeof date !== "string") {
+          console.log(
+            `Item at index ${index} has invalid remindDate format at index ${i}: Not a string`,
+          )
+        }
+      })
+    })
+
+    console.log(excelData)
+    //data của add phương tiện mới
+    const dataNewVehicles = excelData.map((item) => ({
+      license_plate: String(item.license_plate),
+      user_name: String(item.name),
+      license: String(item.phoneNumber),
+      user_address: String(item.address),
+    }))
+    // console.log(dataNewVehicles)
+
+    //data của type dạng array, insert call api từng hàng index 1 vào từ 0
+    //   {
+    //     "license_plate": "1",
+    //     "seri": "avc",
+    //     "size": "50x50",
+    //     "brand": "toyota"
+    // }
+    //đây là 1 ví dụ của 1 object parsedRemindTireData
+    const parsedRemindTireData = excelData.flatMap((item) =>
+      item.remindTire
+        .filter((tire: any) => tire)
+        .map((tire: any) => {
+          const [seri, size, brand] = tire.split(",")
+          return {
+            license_plate: item.license_plate?.toString() || "Unknown",
+            seri,
+            size,
+            brand,
+          }
+        }),
+    )
+    // console.log(parsedRemindTireData);
+    // tạo 
+    const convertToUnix = (dateString: string): any => {
+      if(dateString === null || dateString === undefined || dateString === '') return '';
+      const [datePart, timePart] = dateString.trim().split(" ")
+      const [day, month, year] = datePart.split("/")
+      const [hours, minutes] = timePart.split(":")
+
+      const date = new Date(
+        Number(year),
+        Number(month) - 1,
+        Number(day),
+        Number(hours),
+        Number(minutes),
+      )
+      return Math.floor(date.getTime() / 1000) // Chuyển sang big INT (Unix timestamp)
+    }
+    const formattedData = excelData.map((item) => ({
+      remind_category_id: item.license_plate,
+      expiration_time: convertToUnix(item.exp),
+      cycle: item.cycle,
+      note_repair: item.indexDesc,
+      schedules: item.remindDate
+        .filter((dateStr:any) => dateStr) // Lọc các giá trị null hoặc undefined
+        .map((dateStr:any) => {
+          const [startDate, endDateTime] = dateStr.split("-")
+          const startDateTime = `${startDate.trim()} ${endDateTime
+            .split(" ")[1]
+            .trim()}`
+          const endDateTimeFull = endDateTime.trim();
+          const timeOnly = endDateTime.split(' ')[1].trim();
+          return {
+            start: convertToUnix(startDateTime), 
+            end: convertToUnix(endDateTimeFull), 
+            time: timeOnly, 
+          }
+        }),
+      vehicles: [item.license_plate?.toString()],
+    }))
+    console.log(formattedData)
+  }
 
   return (
     <div>
       <p className="flex items-center">
         Nếu chưa có file exel mẫu,tải mẫu tại đây{" "}
-        <Button icon={<FileExcelOutlined />} type="link" href="/assets/files/template.xlsx">
+        <Button
+          icon={<FileExcelOutlined />}
+          type="link"
+          href="/assets/files/template.xlsx"
+        >
           File{" "}
         </Button>
       </p>
 
       <div className="flex justify-center mt-5 mb-10">
-        <UploadExel setIsUpload={setIsUpload} />
+        <UploadExel setExcelData={setExcelData} setIsUpload={setIsUpload} />
       </div>
       <div className="actions flex justify-end">
         <Button
@@ -33,9 +137,8 @@ const ImportExel: FC<{
           }}
         >
           Hủy
-          
         </Button>
-        <Button disabled={!isUpload} type="primary">
+        <Button onClick={handleImport} disabled={!isUpload} type="primary">
           {" "}
           import{" "}
         </Button>
@@ -45,7 +148,6 @@ const ImportExel: FC<{
 }
 
 const ModalImportExel: FC<ModalImportExelProps> = ({ button }) => {
-  
   return (
     <ModalCView
       modalProps={{ width: 500 }}
